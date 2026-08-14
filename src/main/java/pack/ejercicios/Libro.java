8c9e2bd4afd50f0f2d472e1b60063dc9b4719f21
Ignacia <ie.rojas@duocuc.cl>
2026-08-14 19:13:12 -0400

    listo

--- src/main/java/pack/ejercicios/Libro.java
+++ src/main/java/pack/ejercicios/Libro.java
@@ -1,15 +1,31 @@
 package pack.ejercicios;
 
+//se dejo afuera el numero de paginas, la editorial y el genero porque no son necesarias para el ojetivo del sistema
 public class Libro {
     String titulo;
-    String autor;
+    String author;
     int anio;
+    boolean disponible;
 
-    public Libro(String titulo, String autor, int anio) {
+    public Libro(String titulo, String author, int anio, boolean disponible) {
+        this.titulo = titulo;
+        this.author = author;
+        this.anio = anio;
+
+
 
     }
-    public void leer() {
-        System.out.println("Ingrese el titulo del libro: ");
+    public void prestar() {
+      this.disponible = false;
+
+    }
+
+    public void devolver() {
+        this.disponible = true;
+    }
+
+    public boolean isDisponible() {
+        return disponible;
     }
 }
 
