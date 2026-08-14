32fe31687d4a0dc214d2c2d4673feaa645c06997
Ignacia <ie.rojas@duocuc.cl>
2026-08-14 18:38:37 -0400

    First commit

---
+++ src/main/java/pack/ejercicios/Libro.java
@@ -0,0 +1,15 @@
+package pack.ejercicios;
+
+public class Libro {
+    String titulo;
+    String autor;
+    int anio;
+
+    public Libro(String titulo, String autor, int anio) {
+
+    }
+    public void leer() {
+        System.out.println("Ingrese el titulo del libro: ");
+    }
+}
+
