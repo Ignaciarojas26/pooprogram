77815ccbe881eff0036ef10b45274c06c0d75304
Ignacia <ie.rojas@duocuc.cl>
2026-08-16 14:38:34 -0400

    listo

---
+++ src/main/java/pack/ejercicios/CuentaBancaria.java
@@ -0,0 +1,26 @@
+package pack.ejercicios;
+
+public class CuentaBancaria {
+    String titularCuenta;
+    String tipoDeCuenta;
+    int numeroCuenta;
+    double saldoCuenta;
+
+    public CuentaBancaria(String titularCuenta, String tipoDeCuenta, int numeroCuenta, double saldoCuenta) {
+        this.titularCuenta = titularCuenta;
+        this.tipoDeCuenta = tipoDeCuenta;
+        this.numeroCuenta = numeroCuenta;
+        this.saldoCuenta = 0.0;
+    }
+
+    public double consultarSaldo() {
+        return saldoCuenta;
+    }
+    public void depositar(double monto) {
+        this.saldoCuenta = this.saldoCuenta + monto;
+    }
+    public void girar(double monto) {
+        this.saldoCuenta = this.saldoCuenta - monto;
+    }
+}
+//se dejaron fuera los datos de fecha de apertura, contraseña o surcursal al no ser de caracter basico
--- src/main/java/pack/ejercicios/Libro.java
+++ src/main/java/pack/ejercicios/Libro.java
@@ -1,19 +1,21 @@
 package pack.ejercicios;
 
-//se dejo afuera el numero de paginas, la editorial y el genero porque no son necesarias para el ojetivo del sistema
+//se dejó afuera el numero de páginas, la editorial y el género porque no son necesarias para el ojetivo del sistema
+
+//clase propuesta y atributos
 public class Libro {
     String titulo;
     String author;
     int anio;
     boolean disponible;
 
+    //constructor
     public Libro(String titulo, String author, int anio, boolean disponible) {
         this.titulo = titulo;
         this.author = author;
         this.anio = anio;
 
-
-
+//metodos
     }
     public void prestar() {
       this.disponible = false;
